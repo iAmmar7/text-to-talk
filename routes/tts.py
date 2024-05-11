e7baa563618702daddfcca639988bd02803a80f7
Ammar Ansari <iammaransari@gmail.com>
2024-05-12 00:28:20 +0200

    add comments

--- routes/tts.py
+++ routes/tts.py
@@ -1,10 +1,10 @@
 from flask import request, jsonify, send_file
-from transformers import AutoProcessor, AutoModelForTextToWaveform
+from transformers import AutoProcessor, BarkModel
 import scipy
 
 # Load processor and model from Hugging Face
 processor = AutoProcessor.from_pretrained("suno/bark")
-model = AutoModelForTextToWaveform.from_pretrained("suno/bark")
+model = BarkModel.from_pretrained("suno/bark")
 
 
 def text_to_speech():
@@ -18,11 +18,16 @@ def text_to_speech():
     text = data['text']
     voice_preset = "v2/en_speaker_6"
 
+    # Process the input text with a specific voice preset to prepare it for the model.
     inputs = processor(text, voice_preset=voice_preset)
 
+    # Generate audio from the processed input using the model.
     audio_array = model.generate(**inputs)
+
+    # Move the generated audio tensor to CPU, convert to NumPy array, and remove extra dimensions.
     audio_array = audio_array.cpu().numpy().squeeze()
 
+    # Retrieve the sample rate for the generated audio from the model's configuration.
     sample_rate = model.generation_config.sample_rate
     output = "output.wav"
 
