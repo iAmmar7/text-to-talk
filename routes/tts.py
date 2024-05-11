9f2dd64469b7c05299ea084fb49220bdb5a9b4cf
Ammar Ansari <iammaransari@gmail.com>
2024-05-12 00:07:46 +0200

    use the transformers model

--- routes/tts.py
+++ routes/tts.py
@@ -1,33 +1,32 @@
 from flask import request, jsonify, send_file
-from transformers import AutoProcessor, BarkModel
+from transformers import AutoProcessor, AutoModelForTextToWaveform
 import scipy
 
 # Load processor and model from Hugging Face
 processor = AutoProcessor.from_pretrained("suno/bark")
-model = BarkModel.from_pretrained("suno/bark")
+model = AutoModelForTextToWaveform.from_pretrained("suno/bark")
 
-model.to("cpu")  # Using CPU here as CUDA is not enabled
 
 def text_to_speech():
     if not request.is_json:
-        return jsonify({ "error": "Request must be JSON" }), 400
+        return jsonify({"error": "Request must be JSON"}), 400
 
     data = request.get_json()
     if 'text' not in data:
         return jsonify({"error": "No text provided"}), 400
 
     text = data['text']
-    preset = "v2/en_speaker_9"
+    voice_preset = "v2/en_speaker_6"
+
+    inputs = processor(text, voice_preset=voice_preset)
+
+    audio_array = model.generate(**inputs)
+    audio_array = audio_array.cpu().numpy().squeeze()
+
+    sample_rate = model.generation_config.sample_rate
     output = "output.wav"
 
-    input = processor(text, voice_preset=preset)
-    input = {k: v.to("cpu") for k, v in input.items()}  # Move all tensors to CPU
-    audio_array = model.generate(**input)
-    audio_array = audio_array.cpu().numpy().squeeze()
-    sample_rate = model.generation_config.sample_rate
     scipy.io.wavfile.write(output, rate=sample_rate, data=audio_array)
 
     # Return the audio file
     return send_file(output, as_attachment=True)
-
-    # return jsonify({ "response": text })
